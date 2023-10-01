0316024c45d46c3cef793286843dc662cea121ec
David Mórász <m@mcro.de>
2023-10-01 14:04:15 +0200

    added PNG rendering

--- src/Program.cs
+++ src/Program.cs
@@ -7,14 +7,7 @@ using PowerArgs;
 namespace TablerUtils;
 public class Program
 {
-    public class DrawIOArgs : SvgArgs
-    {
-        [ArgDefaultValue("Tabler Icons"), ArgShortcut("-l")]
-        public string LibraryPrefix { get; set; } = "Tabler Icons";
-    }
-
-    [ArgActionMethod]
-    public void DrawIO(DrawIOArgs args)
+    private static Dictionary<string, TagsEntry> GetTags(SvgArgs args)
     {
         var tagsJson = Path.Combine(args.InputFolder, "tags.json");
         if (!File.Exists(tagsJson))
@@ -30,14 +23,11 @@ public class Program
         {
             throw new Exception("Couldn't parse the tags file");
         }
-        var categories = tags.Values.DistinctBy(t => t.category).Select(t => t.category);
-        Console.WriteLine("Categories:");
-        foreach(var category in categories)
-        {
-            Console.WriteLine("  -" + category);
-        }
+        return tags;
+    }
 
-        Console.WriteLine("Collecting SVG files");
+    private static (IEnumerable<string> svgFiles, string svgTemp, ProgressBar progress) GetSvgFiles(SvgArgs args)
+    {
         var svgFiles = Directory.EnumerateFiles(args.InputFolder, "*.svg").ToList();
         var progress = new ProgressBar(PbStyle.DoubleLine, svgFiles.Count);
         var svgTemp = Path.Combine(args.OutputFolder, ".svgTemp");
@@ -46,13 +36,82 @@ public class Program
         {
             Directory.CreateDirectory(svgTemp);
         }
+        return (svgFiles, svgTemp, progress);
+    }
+
+    private static (Svg svg, string fileOut) RenderSvg(SvgArgs args, string file, string svgTemp)
+    {
+        var svgOut = Svg.Load(file).WithStyling(new(args.Color, args.Stroke));
+        var fileOut = Path.Combine(svgTemp, Path.GetFileName(file));
+        File.WriteAllText(fileOut, svgOut.Serialize());
+        return (svgOut, fileOut);
+    }
+
+    public class RenderToPngArgs : SvgArgs
+    {
+        [ArgDefaultValue("24"), ArgShortcut("-r")]
+        public int Resize { get; set; } = 24;
+    }
+
+    [ArgActionMethod]
+    public void Png(RenderToPngArgs args)
+    {
+        Console.WriteLine("Initializing ImageMagick");
+        MagickNET.Initialize();
+        Console.WriteLine("Collecting SVG files");
+        var (svgFiles, svgTemp, progress) = GetSvgFiles(args);
+        
+        if (!Directory.Exists(args.OutputFolder))
+        {
+            Directory.CreateDirectory(args.OutputFolder);
+        }
+        int currentProgress = 0;
+
+        svgFiles.AsParallel().ForAll(file =>
+        {
+            Interlocked.Add(ref currentProgress, 1);
+            progress.Refresh(currentProgress, Path.GetFileName(file));
+            var (svgOut, fileOut) = RenderSvg(args, file, svgTemp);
+
+            var imageIn = new MagickImage(fileOut, new MagickReadSettings
+            {
+                BackgroundColor = new MagickColor(0, 0, 0, 0),
+                Width = args.Resize,
+                Height = args.Resize,
+                ColorSpace = ColorSpace.sRGB
+            });
+
+            imageIn.Write(
+                Path.Combine(args.OutputFolder, Path.GetFileNameWithoutExtension(file) + ".png")
+            );
+        });
+    }
+
+    public class DrawIOArgs : SvgArgs
+    {
+        [ArgDefaultValue("Tabler Icons"), ArgShortcut("-l")]
+        public string LibraryPrefix { get; set; } = "Tabler Icons";
+    }
+
+    [ArgActionMethod]
+    public void DrawIO(DrawIOArgs args)
+    {
+        var tags = GetTags(args);
+        var categories = tags.Values.DistinctBy(t => t.category).Select(t => t.category);
+        Console.WriteLine("Categories:");
+        foreach(var category in categories)
+        {
+            Console.WriteLine("  -" + category);
+        }
+
+        Console.WriteLine("Collecting SVG files");
+        var (svgFiles, svgTemp, progress) = GetSvgFiles(args);
 
         List<LibraryEntry> allEntries = svgFiles
             .Select(file =>
             {
                 progress.Next(Path.GetFileName(file));
-                var svgOut = Svg.Load(file).WithStyling(new(args.Color, args.Stroke));
-                File.WriteAllText(Path.Combine(svgTemp, Path.GetFileName(file)), svgOut.Serialize());
+                var (svgOut, _) = RenderSvg(args, file, svgTemp);
                 return LibraryEntry.Make(svgOut, Path.GetFileNameWithoutExtension(file));
             })
             .ToList();
